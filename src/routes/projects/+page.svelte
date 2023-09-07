b7f65422e609ec851b3b6c73a6e3256d38e7495e
JustCallMeSimon <github@jcms.dev>
2023-09-07 22:12:34 +0200

    add placeholder sites

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import BackButton from "../backButton.svelte";
+</script>
+
+<body>
+	<h2>soon</h2>
+	<BackButton />
+</body>
+
+<style>
+	body {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+
+	h2 {
+		font-family: roboto;
+		color:white;
+		font-size: 8vw;
+		margin-bottom: 2vmin;
+	}
+
+	h2::after {
+		position: absolute;
+		content: "™";
+	}
+</style>

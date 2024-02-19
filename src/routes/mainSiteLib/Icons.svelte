93374f72015ae8513ece5fb0cc7e11ee9d8da03d
Simon <72569478+JCMSimon@users.noreply.github.com>
2024-02-19 19:15:21 +0100

    Change git link, maybe get like a custom permalink setup for links

--- src/routes/mainSiteLib/Icons.svelte
+++ src/routes/mainSiteLib/Icons.svelte
@@ -1,7 +1,7 @@
 
 <div class="Icons">
 	<a class="icon" href="https://dc.jcms.dev" target="_blank"><img class="iconImg" src="icons/discord.svg" alt="Join my discord server!"></a>
-	<a class="icon" href="https://github.com/JustCallMeSimon26" target="_blank"><img class="iconImg" src="icons/github.svg" alt="Take a look at what im coding."></a>
+	<a class="icon" href="https://github.com/JCMSimon" target="_blank"><img class="iconImg" src="icons/github.svg" alt="Take a look at what im coding."></a>
 	<a class="icon" href="https://twitter.com/JustCallSimon" target="_blank"><img class="iconImg" src="icons/twitter.svg" alt="Read my (rare) twee- ehh i mean X's"></a>
 	<a class="icon" href="https://youtube.com/@JCMS_" target="_blank"><img class="iconImg" src="icons/youtube.svg" alt="Watch my non-existend videos."></a>
 	<a class="icon" href="https://twitch.tv/JCMSimon" target="_blank"><img class="iconImg" src="icons/twitch.svg" alt="And my non-existend stream!"></a>

a56fecb6d1177a079d5925714517007b0d9ce58a
Xeimyn <Xeimyn@users.noreply.github.com>
2025-04-15 22:57:23 +0000

    deploy: 953a7c7eaef1e47423b23e23322c509276af0785

---
+++ _app/immutable/nodes/3.2a98904e.js
@@ -0,0 +1,3 @@
+import{s as l,k as f,n as r}from"../chunks/scheduler.b909ebf8.js";import{S as h,i as u,m as _,g as p,n as d,h as x,j as E,f as s,k as m,a as c}from"../chunks/index.64cdb55c.js";function v(o){let a,e,n;return{c(){a=_(`YEAR
+`),e=p("iframe"),this.h()},l(t){a=d(t,`YEAR
+`),e=x(t,"IFRAME",{src:!0,frameborder:!0}),E(e).forEach(s),this.h()},h(){f(e.src,n="https://year.jcms.dev")||m(e,"src",n),m(e,"frameborder","0")},m(t,i){c(t,a,i),c(t,e,i)},p:r,i:r,o:r,d(t){t&&(s(a),s(e))}}}class b extends h{constructor(a){super(),u(this,a,null,v,l,{})}}export{b as component};
